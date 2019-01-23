5237ce26e1aad575a6a3438ea6a54a601e5e3a96
Anatoliy Zizda <anatoliy.zizda@thomsonreuters.com>
2019-01-23 17:09:15 +0200

    change

--- src/main/java/bdEvent/AppJava.java
+++ src/main/java/bdEvent/AppJava.java
@@ -1,6 +1,5 @@
-package application;
+package bdEvent;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
 
 public class AppJava {
